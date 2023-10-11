2576e9473bb982dd74bfadecd24c088aab68c8ed
Gabrielgsw <ggermanow279@gmail.com>
2023-10-11 11:10:41 -0300

    criando interface user rep e definindo pk db e colunas

--- src/main/java/br/com/gabrielwanderley/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielwanderley/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.gabrielwanderley.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        // @RequestBody -> para indicar ao spring que o argumento faz parte do body
-        System.out.println(userModel.getUsername());
+    @Autowired // notação para o spring gerenciar o userRepository/autoinicializar
+    private IUserRepository userRepository;
 
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        // @RequestBody -> para indicar ao spring que o argumento faz parte do body
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
--- src/main/java/br/com/gabrielwanderley/todolist/user/UserModel.java
+++ src/main/java/br/com/gabrielwanderley/todolist/user/UserModel.java
@@ -1,12 +1,30 @@
 package br.com.gabrielwanderley.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data // utilizado para definir automaticamente os getters e setters
+@Entity(name = "tb_users") // definindo os elementos da classe UserModel como uma tabela com o nome
+                           // tb_users
 public class UserModel {
+    // Definindo um UUID,que é uma sequencia de numeros e letras, como chave
+    // primária
+    @Id // Notação para denotar uma chave primária como id
+    @GeneratedValue(generator = "UUID") // definindo um valor aleatório do tipo UUID
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp // notação para saber a data em que determinado dado foi inserido
+    private LocalDateTime createdAt;
 
 }
