934218840cedfc9a8db7757d137f480dee24a39b
Gabrielgsw <ggermanow279@gmail.com>
2023-10-12 11:19:42 -0300

    adicionando bcrypt

--- src/main/java/br/com/gabrielwanderley/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielwanderley/todolist/user/UserController.java
@@ -2,12 +2,14 @@ package br.com.gabrielwanderley.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+// import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -26,6 +28,10 @@ public class UserController {
             // Status code
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred); // apos a senha ser definida, a senha deve ser setada como um hash code
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
